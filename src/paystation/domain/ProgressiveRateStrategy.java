879e91745c19167fd70a7faecef8ef2005cb5de9
Philip <pcappelli901@gmail.com>
2017-09-23 11:16:30 -0400

    Implemented rate strategies for three clients: Alphatown, Betatown, and Gammatown

--- src/paystation/domain/ProgressiveRateStrategy.java
+++ src/paystation/domain/ProgressiveRateStrategy.java
@@ -5,8 +5,6 @@ public class ProgressiveRateStrategy implements RateStrategy {
     public int calculateTime(int amount) {
 
         int time = 0;
-        // This algorithm needs to be fixed. Breaks the code.  Verified I am able to switch
-        // rate strategy at run-time at least
         if (amount >= 150 + 200) {
             amount -= 350;
             time = 120 + amount / 5;
