19761d2ff046603634da752d1f8028b6f53f7559
Philip <pcappelli901@gmail.com>
2017-09-24 15:03:20 -0400

    Abstracted out logic of the main driver into a delegate object so main method logic is cleaner.

--- src/paystation/domain/PayStationClient.java
+++ src/paystation/domain/PayStationClient.java
@@ -1,60 +1,37 @@
 package paystation.domain;
-
 import paystation.domain.Strategies.*;
 
-import java.util.Map;
 import java.util.Scanner;
 
 /**
- * This is just a client-driver simulation for the PayStation which uses methods
+ * This is a client-driver simulation for the PayStation which uses methods
  * derived by the PayStation interface and the PayStationImpl object.
  *
  * Written by: Philip Cappelli
  */
 public class PayStationClient {
-
     public static void main(String[] args) {
-
         PayStationImpl payStation = new PayStationImpl();
+        PayStationDelegate delegate = new PayStationDelegate();
         Scanner input = new Scanner(System.in);
         int selection;
 
         do {
-            displayMenu();
+            delegate.displayMenu();
             selection = input.nextInt();
 
             switch (selection) {
                 case 1:
-                    int coinsBeingAdded;
-                    boolean stillPaying = true;
-
-                    while (stillPaying) {
-                        System.out.println("PayStation only accepts 5, 10, 25 cent values");
-                        System.out.println("Enter 0 when complete ");
-
-                        coinsBeingAdded = input.nextInt();
-                        if (coinsBeingAdded == 0) {
-                            stillPaying = false;
-                        } else {
-                            try {
-                                payStation.addPayment(coinsBeingAdded);
-                            } catch (IllegalCoinException e) {
-                                System.out.println("Invalid coin, ill let it slide this one time...");
-                            }
-                        }
-                    }
+                    delegate.addCoinsBeingInserted(payStation);
                     break;
                 case 2:
-                    int time = payStation.readDisplay();
-                    System.out.println("Current time purchased: " + time);
+                    delegate.readDisplay(payStation);
                     break;
                 case 3:
-                    Receipt receipt = payStation.buy();
-                    System.out.println(receipt.value());
+                    delegate.printReceipt(payStation);
                     break;
                 case 4:
-                    Map<Integer, Integer> cancelMap = payStation.cancel();
-                    System.out.println(cancelMap);
+                    delegate.printCancelMapOfCoins(payStation);
                     break;
                 case 5:
                     System.out.println("Enter 1 for AlphaTown, 2 for Betatown, or 3 for GamaTown: ");
@@ -70,18 +47,12 @@ public class PayStationClient {
                                                                                     new ClockBasedDecisionStrategy()));
                     }
                     break;
+                case 6:
+                    break;
                 default:
-                    System.out.println("Please select only between options 1 through 4");
+                    System.out.println("Please select only between options 1 through 6");
             }
-        } while (selection != 4);
-    }
-
-    private static void displayMenu() {
-        System.out.println("1. Deposit Coins");
-        System.out.println("2. Display");
-        System.out.println("3. Buy Ticket");
-        System.out.println("4. Cancel");
-        System.out.println("5. Change Rate Strategy");
+        } while (selection != 6);
     }
 }
 
---
+++ src/paystation/domain/PayStationDelegate.java
@@ -0,0 +1,54 @@
+package paystation.domain;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class PayStationDelegate {
+    public  void displayMenu() {
+        System.out.println("1. Deposit Coins");
+        System.out.println("2. Display");
+        System.out.println("3. Buy Ticket");
+        System.out.println("4. Cancel");
+        System.out.println("5. Change Rate Strategy");
+        System.out.println("6. Shutdown PayStation");
+    }
+
+    public void readDisplay(PayStation payStation) {
+        int time = payStation.readDisplay();
+        System.out.println("Current time purchased: " + time);
+    }
+
+    public  void addCoinsBeingInserted(PayStation payStation) {
+        Scanner input = new Scanner(System.in);
+        int coinsBeingAdded;
+        boolean stillPaying = true;
+
+        while (stillPaying) {
+            System.out.println("PayStation only accepts 5, 10, 25 cent values");
+            System.out.println("Enter 0 when complete ");
+            coinsBeingAdded = input.nextInt();
+
+            if (coinsBeingAdded == 0) {
+                stillPaying = false;
+            } else {
+                try {
+                    payStation.addPayment(coinsBeingAdded);
+                } catch (IllegalCoinException e) {
+                    System.out.println("Invalid coin, ill let it slide this one time...");
+                }
+            }
+        }
+    }
+
+    public void printReceipt(PayStation payStation) {
+        Receipt receipt = payStation.buy();
+        System.out.println("\n$$$ PayStation Parking Receipt $$$");
+        System.out.println("---------- Time: " + receipt.value() + " mins ----------");
+        System.out.println("     :)1 Have A Great Day! :)\n");
+    }
+
+    public void printCancelMapOfCoins(PayStation payStation) {
+        Map<Integer, Integer> cancelMap = payStation.cancel();
+        System.out.println(cancelMap);
+    }
+}
